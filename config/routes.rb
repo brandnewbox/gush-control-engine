5233bd9e4e2b9439265fd3018fcf4fe9af57f22f
Nathan Colgate <nathancolgate@users.noreply.github.com>
2019-01-29 14:31:08 -0600

    Allowing users to mount it where they please
    Addresses #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
-Rails.application.routes.draw do
-  mount Gush::Control::Engine::Engine, at: "/gush"
-end
-
 Gush::Control::Engine::Engine.routes.draw do
-
   resources :workflows, only: [:index, :show] do
     post :purge_all, on: :collection
     post :purge, on: :member
